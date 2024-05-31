1dc96d8af480402e9d666bd13733620e48236867
Kura <kura.hax@gmail.com>
2024-05-31 13:30:23 +0100

    Remove requests, use retries

--- price_per_kwh.py
+++ price_per_kwh.py
@@ -1,17 +1,15 @@
 import re
+import subprocess
 from datetime import datetime
 from io import StringIO
 
 import pandas
-import requests
+from retry import retry
 
 
 URL = "https://www.moneysavingexpert.com/utilities/what-are-the-price-cap-unit-rates-/"
 HEADERS = {
-    "User-Agent": (
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-        "(KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
-    )
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 14.4; rv:124.0esr) Gecko/20000101 Firefox/124.0esr"
 }
 KWH_PAT = r"Unit rate: ([0-9\.]*)p per kWh"
 STDC_PAT = r"Standing charge: ([0-9\.]*)p per day"
@@ -23,9 +21,15 @@ QUARTERS = {
   **dict.fromkeys([10, 11, 12], "1 october to 31 december"),
 }
 
-try:
-    r = requests.get(URL, headers=HEADERS)
-    table = pandas.read_html(StringIO(r.text))[0]
+
+@retry(Exception, tries=6, delay=10)
+def get_price_cap():
+    r = subprocess.run(
+        ["curl", "-H", "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 14.4; rv:124.0esr) Gecko/20000101 Firefox/124.0esr", URL],
+        capture_output=True,
+        text=True
+    )
+    table = pandas.read_html(StringIO(r.stdout))[0]
 
     idx = [
         i
@@ -37,7 +41,7 @@ try:
     price_cap = None
     for i in range(1, len(table)):
         heading = table[i][0].lower()
-        if "current energy price cap" in heading and current_quarter in heading:
+        if "energy price cap" in heading and current_quarter in heading:
             price_cap = table[i][idx]
     if not price_cap:
         raise Exception("Couldn't find price cap")
@@ -45,15 +49,19 @@ try:
     kwh_res = re.search(KWH_PAT, price_cap)
     stand_chrg_res = re.search(STDC_PAT, price_cap)
 
-    if kwh_res and stand_chrg_res:
-        kwh_price, stand_chrg_price = (
-            float(kwh_res.group(1)) / 100,
-            float(stand_chrg_res.group(1)) / 100,
-        )
+    try:
+        kwh_price = float(kwh_res.group(1)) / 100
+        stand_chrg_price = float(stand_chrg_res.group(1)) / 100
+    except:
+        raise Exception("Couldn't convert to float")
+
+    return kwh_price, stand_chrg_price
+
+
+if __name__ == "__main__":
+    kwh_price, stand_chrg_price = get_price_cap()
+    if kwh_price and stand_chrg_price:
         print(
             f"p110_energy_price price_per_kwh={kwh_price},"
             f"standing_charge={stand_chrg_price}"
         )
-
-except Exception as e:
-    pass
