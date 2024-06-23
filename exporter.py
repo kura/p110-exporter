1b5c98bec479d1b7f2a07303f984b10fcb683029
Kura <kura.hax@gmail.com>
2024-06-23 17:43:24 +0100

    Add Tapo 310 support

--- exporter.py
+++ exporter.py
@@ -1,6 +1,7 @@
+import asyncio
 import time
 
-from PyP100 import PyP110
+from tapo import ApiClient
 from yaml import safe_load
 
 
@@ -31,57 +32,85 @@ devices:
   - name: tv
     room: living_room
     ip: 10.0.80.5
-"""
 
-daily_energy = []
-monthly_energy = []
+hub:
+  ip: 10.0.80.254
+"""
 
 cfg = safe_load(CFG)
 TAPO_P110_WATTAGE = float(cfg["tapo_wattage"])
-for device in cfg["devices"]:
-    dev = PyP110.P110(device["ip"], cfg["auth"]["user"], cfg["auth"]["password"])
+daily_energy = monthly_energy = 0
 
-    res = dev.getEnergyUsage()
 
-    try:
-        # name=rack,
-        # room=office,
-        # ip=10.0.80.2
-        tags = ",".join([f"{k}={v}" for k, v in device.items()])
+async def tapo_p110(device):
+    global daily_energy, monthly_energy
+    api_client = ApiClient(cfg.get("auth").get("user"), cfg.get("auth").get("password"))
+    dev = await api_client.p110(device.get("ip"))
+    res = await dev.get_energy_usage()
+    data = res.to_dict()
 
-        for k, v in res.items():
-            if k == "current_power":
-                res[k] += TAPO_P110_WATTAGE
-            if k == "today_energy":
-                res[k] += (
-                    TAPO_P110_WATTAGE * max(0, time.localtime().tm_hour - 1)
-                ) / 1000
-            if k == "month_energy":
-                res[k] += (
-                    TAPO_P110_WATTAGE * (max(0, time.localtime().tm_mday - 1) * 24)
-                ) / 1000
-                res[k] += (
-                    TAPO_P110_WATTAGE * max(0, time.localtime().tm_hour - 1)
-                ) / 1000
+    for k, v in data.items():
+        if k == "current_power":
+            data[k] += TAPO_P110_WATTAGE
+        if k == "today_energy":
+            data[k] += (
+                TAPO_P110_WATTAGE * max(0, time.localtime().tm_hour - 1)
+            ) / 1000
+        if k == "month_energy":
+            data[k] += (
+                TAPO_P110_WATTAGE * (max(0, time.localtime().tm_mday - 1) * 24)
+            ) / 1000
+            data[k] += (
+                TAPO_P110_WATTAGE * max(0, time.localtime().tm_hour - 1)
+            ) / 1000
 
-        # today_runtime=711i,   # minutes
-        # month_runtime=1197i,  # minutes
-        # today_energy=1758,    # watt-hours (Wh)
-        # month_energy=2928,    # watt-hours (Wh)
-        # current_power=1526    # milliwatts (mW)
-        fields = ",".join(
-            [
-                f"""{k}={round(v)}i"""
-                for k, v in res.items()
-                if k not in ("local_time", "electricity_charge")
-            ]
-        )
-        daily_energy.append(res["today_energy"])
-        monthly_energy.append(res["month_energy"])
+    daily_energy += data["today_energy"]
+    monthly_energy += data["month_energy"]
 
-        print(f"""p110_energy_consumption,{tags} {fields}""")
-    except Exception as e:
-        continue
+    tags = ",".join([f"{k}={v}" for k, v in device.items()])
+    fields = ",".join(
+        [
+            f"""{k}={round(v)}i"""
+            for k, v in data.items()
+            if k not in ("local_time",)
+        ]
+    )
 
-print(f"p110_energy_daily_total total_sum={round(sum(daily_energy))}i")
-print(f"p110_energy_monthly_total total_sum={round(sum(monthly_energy))}i")
+    print(f"p110_energy_consumption,{tags} {fields}")
+
+
+async def tapo_310(device):
+    data = device.to_dict()
+    tags = [f"name={data['nickname']}",]
+    fields = [
+        f"temperature={data['current_temp']}",
+        f"humidity={data['current_humidity']}",
+        f"low_battery={1 if data['at_low_battery'] else 0}i",
+        f"signal={data['signal_level']}i",
+        f"online={1 if data['status'] == 'online' else 0}i",
+    ]
+    tags = ",".join(tags)
+    fields = ",".join(fields)
+    print(f"tapo_310,{tags} {fields}")
+
+
+async def tapo_hub():
+    loop = asyncio.get_running_loop()
+    api_client = ApiClient(cfg.get("auth").get("user"), cfg.get("auth").get("password"))
+    hub = await api_client.h100(cfg.get("hub").get("ip"))
+    devices = await hub.get_child_device_list()
+    for device in devices:
+        loop.create_task(tapo_310(device))
+
+
+loop = asyncio.get_event_loop()
+tasks = [
+    loop.create_task(tapo_p110(device)) for device in cfg.get("devices")
+]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.run_until_complete(asyncio.wait([tapo_hub(),]))
+loop.close()
+
+
+print(f"p110_energy_daily_total total_sum={round(daily_energy)}i")
+print(f"p110_energy_monthly_total total_sum={round(monthly_energy)}i")
